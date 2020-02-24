332453746b5138ca1cd69905bbcfa8bb70c52fab
noopy <young_ahn@yonsei.ac.kr>
2020-02-24 10:54:36 +0900

    separated functions to video, user controllers from video, user routers

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,3 +1,10 @@
 // exporting and sends function to globalRouter.js
 export const home = (req, res) => res.send("video home");
 export const search = (req, res) => res.send("video search");
+
+// exporting and sends function to videoRouter.js
+export const videos = (req, res) => res.send("videos");
+export const upload = (req, res) => res.send("upload");
+export const videoDetail = (req, res) => res.send("video detail");
+export const editVideo = (req, res) => res.send("edit video");
+export const deleteVideo = (req, res) => res.send("delete video");
